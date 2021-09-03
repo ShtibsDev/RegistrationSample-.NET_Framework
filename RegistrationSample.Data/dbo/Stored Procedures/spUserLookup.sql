ea3aa2db0314dddf72112d85265d2bd63e3328b0
Shtibs <56930268+Shtibs@users.noreply.github.com>
2021-09-03 12:18:26 +0300

    Added Database project.
    Database was created.

---
+++ RegistrationSample.Data/dbo/Stored Procedures/spUserLookup.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spUserLookup]
+	@Id NVARCHAR(128)
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	SELECT FirstName,LastName,EmailAddress, BirthDate, LastLogin
+	FROM dbo.Users
+	WHERE Id = @Id;
+END
